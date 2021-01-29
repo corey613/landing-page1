2c0d4d3b835ed204c3beae5d6359192802bbdb65
Corey <57112820+corey613@users.noreply.github.com>
2021-01-29 18:22:12 +0000

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+     gsap.from(".navContainer", {opacity: 0, duration: 2, delay: 1.5, x: 100, ease: 'elastic'});
+     gsap.from("#headerLeft", { stagger: 2, opacity: 0, duration: 2, y: -100 });
+     gsap.from(".logo", { opacity: 0, duration: 2, x: -100 });
+
+
+
+
+
